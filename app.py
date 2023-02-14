ceaaac9346cfecf3c8b2059b55a260384a60f47e
Rystambek <rustambekjumaqulov@gmail.com>
2023-02-14 11:03:18 +0500

    init

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
-from flask import Flask, request, jsonify
-import telegram
+from flask import Flask, request
+import requests
 import os 
 
-TOKEN = os.environ.get('TOKEN')
-bot = telegram.Bot(Token=TOKEN)
-
 app = Flask(__name__)
-def home():
-    html = '''
-    <h1> This is a home page </h1>
-    <p> This is a paragraph </p>
-    '''
-    print(TOKEN)
-    return html
 
-@app.route('/api', methods= ['POST'])
-def api():
+TOKEN = os.environ.get('TOKEN')
 
+@app.route('/webhook', methods = ['POST'])
+def webhook():
     data = request.get_json(force=True)
-    chat_id = 996172963
-    bot.sendMesage(chat_id = chat_id, text= 'Salom')
-    return jsonify(data)
+    print(data)
 
-if __name__=='__main__':
-    app.run(debug=True)
+    return 'ok'
+
