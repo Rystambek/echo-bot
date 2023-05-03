cadeff68753c12c8a211334ca3096aef00f34c29
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-03 10:32:49 +0500

    flask

--- bot.py
+++ bot.py
@@ -7,11 +7,4 @@ TOKEN= os.environ['TOKEN']
 def start(update:Update,context:CallbackContext):
     update.message.reply_text('hello')
 
-updater = Updater(TOKEN)
 
-dp =  updater.dispatcher
-
-dp.add_handler(CommandHandler('start',start))
-
-updater.start_polling()
-updater.idle()
